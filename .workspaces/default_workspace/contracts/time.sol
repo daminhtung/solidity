b52a2cd836393a01e9a3b7fd422c20d6942a992c
daminhtung <daminhtung@gmail.com>
2022-04-17 16:29:02 +0700

    solidity - time

---
+++ .workspaces/default_workspace/contracts/time.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Game {
+
+    uint public countPlayer = 0;
+    mapping (address => Player) public players;
+    enum Level { Beginner, Intermadiate, Avande }
+
+    struct Player {
+        string fullName;
+        uint age;
+        string sex;
+        Level myLevel;
+        uint createTime;
+    }
+
+    function addPlayer(string memory fullName, uint age, string memory sex) public {
+        players[msg.sender] = Player(fullName, age, sex, Level.Beginner, block.timestamp);
+        countPlayer += 1;
+    }
+
+    function getPlayerLevel(address player) public returns (Level) {
+        return players[player].myLevel;
+    }
+
+    function changePlayerLevel(address player) public {
+        Player storage player = players[player];
+        if (block.timestamp >= player.createTime + 15) { //After 15s
+            player.myLevel = Level.Intermadiate;
+        }
+    }
+}
